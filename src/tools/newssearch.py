b82727fcc22f60cd8f32efda94c489b850dee913
willtwr <wrtan.edu@gmail.com>
2025-03-24 15:25:51 +0800

    updated summarizer prompt and use selenium

--- src/tools/newssearch.py
+++ src/tools/newssearch.py
@@ -1,4 +1,4 @@
-import requests
+from selenium import webdriver
 from duckduckgo_search import DDGS
 from bs4 import BeautifulSoup
 from langchain.tools import tool
@@ -11,15 +11,19 @@ def news_search(query: str) -> str:
         results = ddgs.news(query, max_results=5)
 
     output = []
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome(options=options)
     for result in results:
-        try:
-            response = requests.get(result['url'], timeout=5)
-        except requests.exceptions.ReadTimeout:
-            output.append("#" + result['title'])
-            continue
-        finally:
-            soup = BeautifulSoup(response.content, 'html.parser')
-            output.append("#" + result['title'] + "\n\n" + "\n\n".join([x for x in soup.get_text().strip().splitlines() if bool(x)]))
+        driver.get(result['url'])
+        response = driver.page_source
+        print(response)
+        soup = BeautifulSoup(response, 'html.parser')
+        output.append("#" + result['title'] + "\n\n" + "\n\n".join([x for x in soup.get_text().strip().splitlines() if bool(x)]))
+
+    driver.quit()
 
     output = '\n\n'.join(output)
     return output
