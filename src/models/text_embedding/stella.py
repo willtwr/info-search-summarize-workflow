6cf79ff52b63099c70e5b798c40c9f5311b9b1dd
willtwr <wrtan.edu@gmail.com>
2025-03-26 11:39:27 +0800

    added stella embedding model

--- src/models/text_embedding/stella.py
+++ src/models/text_embedding/stella.py
@@ -3,12 +3,12 @@ from sentence_transformers import SentenceTransformer
 
 
 class Stella:
-    """Stella embedding model"""
+    """Stella EN 1.5B v5 embedding model"""
     def __init__(self):
         self.query_prompt_name = "s2p_query"
         self.build_model()
 
-    def build_model(self):
+    def build_model(self) -> None:
         self.model = SentenceTransformer(
             "dunzhang/stella_en_1.5B_v5",
             trust_remote_code=True
